20c8874a56adeb90cf0055367c16cc730966cd57
twiceChoi <a01037585725a@gmail.com>
2022-09-23 21:17:32 +0900

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,18 +40,7 @@ function blog_posts() {
                 url: 'https://docs.google.com/viewer?url=https://github.com/twiceChoi/twiceChoi.github.io/blob/main/assets/images/project_2.pdf?raw=T',
                 title: 'Privacy - How I Converted',
             },
-            {
-                url: 'https://www.nagekar.com/2018/01/jagriti-yatra.html',
-                title: 'Jagriti Yatra 2017',
-            },
-            {
-                url: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-                title: 'Private Cloud Part 2 | Encrypted Storage With NextCloud',
-            },
-            {
-                url: 'https://www.nagekar.com/2018/07/eli5-how-https-works.html',
-                title: 'ELI5 - How HTTPS Works',
-            },
+
         ];
     let post_html = [];
 
