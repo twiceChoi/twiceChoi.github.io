06a6cf0b425dc22e2176534d3b3f904cc255b119
twiceChoi <a01037585725a@gmail.com>
2022-09-24 03:22:02 +0900

    add tech

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,21 +20,10 @@ function general_utils() {
 
 function blog_posts() {
 
-
-    // let posts = [
-    //     {
-    //         url: 'https://docs.google.com/viewer?url=https://github.com/twiceChoi/twiceChoi.github.io/blob/main/assets/images/project_1.pdf?raw=T',
-    //         ttitle: 'Trek To Bramhatal (Uttarakhand)',
-    //     },
-    //     {
-    //         url: 'https://docs.google.com/viewer?url=https://github.com/twiceChoi/twiceChoi.github.io/blob/main/assets/images/project_2.pdf?raw=T',
-    //         title: 'Privacy - How I Converted',
-    //     },
-    // ];
     let posts = [
             {
                 url: 'https://docs.google.com/viewer?url=https://github.com/twiceChoi/twiceChoi.github.io/blob/main/assets/images/project_1.pdf?raw=T',
-                title: 'Trek To Bramhatal (Uttarakhand)',
+                title: 'It takes two 기술소개서',
             },
             {
                 url: 'https://docs.google.com/viewer?url=https://github.com/twiceChoi/twiceChoi.github.io/blob/main/assets/images/project_2.pdf?raw=T',
@@ -69,20 +58,6 @@ function blog_posts() {
 
         post_html.push(post_template);
     }
-
-        // for the more posts link
-    // let post_template = `
-    // <div class="blog-post more-blogs" onclick="blog_link_click('https://www.nagekar.com');">
-    //     <div class="blog-link">
-    //         <h3><a href="https://www.nagekar.com">Visit the blog for more posts</a></h3>            
-    //     </div>
-    //     <div class="blog-goto-link">
-    //         <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
-    //     </div>
-    // </div>
-    // `;
-
-    // post_html.push(post_template);
 
     $('#rss-feeds').html(post_html);
 }
