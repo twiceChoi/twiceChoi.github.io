3840aad949ae43a7d8c5f13a529a2000a5707137
twiceChoi <a01037585725a@gmail.com>
2022-09-23 15:06:48 +0900

    add pdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,36 +22,16 @@ function blog_posts() {
 
 
     let posts = [
-       // {
-            //url: 'https://twicechoi.github.io/images/project_1.pdf',
-            //title: '3D 개인 포트폴리오',
-       // },
-        //{
-            //url: 'https://twicechoi.github.io/images/project_2.pdf',
-            //title: '3D 팀 포트폴리오',
-        //},
-
         {
             url: 'https://docs.google.com/viewer?url=https://github.com/twiceChoi/twiceChoi.github.io/blob/main/assets/images/project_1.pdf?raw=T',
-            title: 'Trek To Bramhatal (Uttarakhand)',
+            title: '3D 개인 포트폴리오',
         },
         {
-            url: 'https://www.nagekar.com/2017/08/privacy.html',
-            title: 'Privacy - How I Converted',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/01/jagriti-yatra.html',
-            title: 'Jagriti Yatra 2017',
-        },
-        {
-            url: 'https://www.nagekar.com/2017/08/private-cloud-part-2.html',
-            title: 'Private Cloud Part 2 | Encrypted Storage With NextCloud',
-        },
-        {
-            url: 'https://www.nagekar.com/2018/07/eli5-how-https-works.html',
-            title: 'ELI5 - How HTTPS Works',
+            url: 'https://docs.google.com/viewer?url=https://github.com/twiceChoi/twiceChoi.github.io/blob/main/assets/images/project_2.pdf?raw=T',
+            title: '3D 팀 포트폴리오',
         },
     ];
+
     let post_html = [];
 
     for(let post of posts) {
@@ -79,26 +59,7 @@ function blog_posts() {
 
             post_html.push(post_template);
         }
-
-        // for the more posts link
-        let post_template = `
-        <div class="blog-post more-blogs" onclick="blog_link_click('https://www.nagekar.com');">
-            <div class="blog-link">
-                <h3><a href="https://www.nagekar.com">Visit the blog for more posts</a></h3>            
-            </div>
-            <div class="blog-goto-link">
-                <img class="blog-arrow" src="/assets/images/right-open-mini.svg"/>
-            </div>
-        </div>
-        `;
-
-        post_html.push(post_template);
-
-        $('#rss-feeds').html(post_html);
-
 }
-
-
 function blog_link_click(url) {
     window.location = url;
 }
